4d00a9e2a4677fbd247f97bef06774450839522e
MihhFor~ <fr.mikle@ya.ru>
2023-11-27 19:15:55 +0300

    Fisrt modification

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,8 +2,8 @@ public class CreditPaymentService {
 
     public int calculate(int sum, int period, double percent) {
 
-        int Paymentinmonth = (int) (sum * percent / (12 * 100) / (1 - Math.pow(1 + (percent / (12 * 100)), -(period * 12))));
+        int paymentinmonth = (int) (sum * percent / (12 * 100) / (1 - Math.pow(1 + (percent / (12 * 100)), -(period * 12))));
 
-        return Paymentinmonth;
+        return paymentinmonth;
     }
 }
